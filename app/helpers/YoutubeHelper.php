a361567f9ccb5c002d5dece9ed7cfb20448fc01e
Ramon <raymondjavaxx@gmail.com>
2010-08-11 18:19:40 -0400

    added youtube helper

---
+++ app/helpers/YoutubeHelper.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * RoxPHP YouTube Helper
+ *
+ * Copyright (C) 2010 Ramon Torres
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) 2010 Ramon Torres
+ * @package default
+ * @license The MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/**
+ * YouTube Helper
+ *
+ * @package default
+ */
+class YoutubeHelper {
+
+	/**
+	 * Embeds a youtube video
+	 *
+	 * @param string $vid   video id or video URL
+	 * @param array $options 
+	 * @return string
+	 */
+	public function embed($vid, $options = array()) {
+		$defaults = array('width' => 640, 'height' => 385, 'hd' => false, 'related' => false);
+		$options += $defaults;
+
+		$flashParams = array();
+		$flashParams[] = (strpos($vid, 'http://') === 0) ? self::extractVideoId($vid) : $vid;
+		if ($options['hd']) {
+			$flashParams[] = 'hd=1';
+		}
+
+		if (!$options['related']) {
+			$flashParams[] = 'rel=0';
+		}
+
+		$playerUrl = 'http://www.youtube.com/v/' . htmlentities(implode('&', $flashParams));
+
+		$embedCode = <<<EOD
+<object width="{$options['width']}" height="{$options['height']}">
+	<param name="movie" value="{$playerUrl}"></param>
+	<param name="allowFullScreen" value="true"></param>
+	<param name="allowscriptaccess" value="always"></param>
+	<param name="wmode" value="opaque"></param>
+	<embed src="{$playerUrl}" type="application/x-shockwave-flash" allowscriptaccess="always"
+		allowfullscreen="true" width="{$options['width']}" height="{$options['height']}">
+	</embed>
+</object>
+EOD;
+
+		return $embedCode;
+	}
+
+	/**
+	 * undocumented function
+	 *
+	 * @param string $url 
+	 * @return string
+	 */
+	public static function extractVideoId($url) {
+		$query = parse_url($url, PHP_URL_QUERY);
+		parse_str($query, $parts);
+		return isset($parts['v']) ? $parts['v'] : false;
+	}
+}
